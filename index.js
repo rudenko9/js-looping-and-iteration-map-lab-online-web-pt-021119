26865d96728ba947144e3784491145c2e0d695ef
Nataliya R <n.rudenko@gmail.com>
2019-09-12 02:17:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function  lowerCaseDrivers(list) {
   return list.map(name => name.toLowerCase())
 }
 
+<<<<<<< HEAD
 function split(name) {
    return {firstName: name.split(' ')[0], lastName: name.split(' ')[1]};
 }
@@ -15,4 +16,8 @@ function attributesToPhrase(list) {
   return list.map(function(driver) { 
     return `${driver.name} is from ${driver.hometown}` ;
   })
+=======
+function nameToAttributes(list) {
+  return list.map(value => firstName, lastName)
+>>>>>>> 9a114057fbce5d56bdde15c7a6be32e61aa8d5f2
 }
